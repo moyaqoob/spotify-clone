b7af8e5c3ea09f5b72233fae695a05a185575ad2
moyaqoob <moyaqoob28@gmail.com>
2024-09-13 02:12:54 +0530

    commit

---
+++ react-projects/small-react-projects/tailwind.config.js
@@ -0,0 +1,12 @@
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
+
